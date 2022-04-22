33bdb710f80429aa3a575dafb9c29b64b5ccce5f
Stella Silvaroli <99274582+Stella-Silvaroli06@users.noreply.github.com>
2022-04-22 15:05:12 +0000

    edited

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2022 Stella S All rights reserved
+//
+// Created by: Stella S
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates cups of water you should drink
+ */
+function calculate () {
+ 
+  // get user input
+  let weight = parseFloat(document.getElementById('body_weight').value)
+  let age = parseFloat(document.getElementById('person_age').value)
+
+  // output
+  const water_result_1 = (2.2 / weight)
+
+  // output
+  const water_result_2 = (water_result_1 * 35)
+
+  // output
+  const water = (water_result_2 / 28.3)
+  
+  // display the results
+  document.getElementById('water').innerHTML = 'you should drink: ' + water.toFixed(2) 
+}
